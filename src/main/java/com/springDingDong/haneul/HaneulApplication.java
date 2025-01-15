c3a215fe299dfbb44078d99da99e5be435d195f7
cie10 <leesky0075@dgu.ac.kr>
2025-01-15 09:13:00 +0900

    init setting

---
+++ src/main/java/com/springDingDong/haneul/HaneulApplication.java
@@ -0,0 +1,13 @@
+package com.springDingDong.haneul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaneulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaneulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springDingDong/haneul/HaneulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springDingDong.haneul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaneulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
